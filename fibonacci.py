b4d75e8efbeb2eaca590c6838a3490afc43faa4d
Rashid Ibrahim <rashid.s.ibrahim@gmail.com>
2018-05-16 21:44:34 -0400

    Initial build out of memoization. There is a bug in the code right now, so I need to find and fix it. Sometimes an infinite loop is spawned. Not sure how this is happening but I'll get to it. Right now the memoization only helps on the current run of the script. Eventually I plan on storing the calculated numbers into a file so that the memoization persists over successive runs. This is more of the meat of what I am trying to do a proof of concept on with this now. I want to beable to apply this to other scripts and storing data. This would also help imensly with some of the NLP and machine learning I have been working on.

--- fibonacci.py
+++ fibonacci.py
@@ -1,34 +1,57 @@
 ##################################################################################
 # Author: Rashid "Lee" Ibrahim                                                   #
 # Date: Jan 29, 2018                                                             #
+# Modified: May 05, 2016                                                         #
 # Program: Fibonacci Sequence Calculator                                         #
 # Purpose: Calculates the nth fibonacci number either recursivly or itteratively #
 ##################################################################################
 
+memoizeFib = {}
 
 def fiboRecursive(ans, oldAns, curr, end):
-    if curr != end+1:
+    if curr != end+1: 
         newAns = ans + oldAns
+        memoizeFib[curr] = newAns
         return fiboRecursive(newAns, ans, curr+1, end)
     return (ans + oldAns)
 
 
-def fiboLoop(n):
-    ans = 1
-    oldAns = 0
-    for i in range(1, n+1):
+def fiboLoop(ans, oldAns, n):
+    for i in range(len(memoizeFib)+1, n+1):
+        memoizeFib[i] = oldAns
         oldAns, ans = ans, ans+oldAns
     return ans
 
 
 def main():
-    x = int(input('Enter 1 for recursive fibonacci. Enter 2 for iterative fibonacci. '))
-    y = int(input('What number would you like to calculate the fibonacci sequence out to? '))
-    
-    if x == 1:
-        print('The', str(y), 'fibonacci number is', fiboRecursive(1, 0, 1, y))
-    else:
-        print('The', str(y), 'fibonacci number is', fiboLoop(y))
+    while True:
+        x = int(input('Enter 1 for recursive fibonacci. Enter 2 for iterative fibonacci. '))
+        y = int(input('What number would you like to calculate the fibonacci sequence out to? '))
+
+        print('\n')
+        print('memoizeFib')
+        print(memoizeFib)
+        print('\n')
+        if y in memoizeFib:
+            print('memoize found')
+            print('The {0} fibbonacci number is {1}'.format(y, memoizeFib[y]))
+            print(memoizeFib[8])
+        else:
+            if len(memoizeFib) > 1:
+                ans = memoizeFib[len(memoizeFib)-1]
+                oldAns = memoizeFib[len(memoizeFib)-1]
+            else:
+                ans = 1
+                oldAns = 0
+                
+            if x == 1:
+                print('The', str(y), 'fibonacci number is', fiboRecursive(ans, oldAns, len(memoizeFib)+1, y))
+            else:
+                print('The', str(y), 'fibonacci number is', fiboLoop(ans, oldAns, y))
+
+        z = input('Run again? ')
+        if z != 'y' and z != 'yes':
+            break
     
     return
 
